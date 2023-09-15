9c3e1dfc83c0c643dbcb7615616e2851b4ab57de
8BitGinger <ryan.fann>
2023-09-15 18:26:31 -0500

    working on second tutorial

---
+++ assets/js/newscript.js
@@ -0,0 +1,101 @@
+var questions = [
+    {
+        question: "What is Question 1"
+        answers: [
+            {text: "wrong answer", correct: false},
+            {text: "wrong answer", correct: false},
+            {text: "right answer", correct: true},
+            {text: "wrong answer", correct: false},
+        ]
+    },
+
+    {
+        question: "What is Question 2"
+        answers: [
+            {text: "wrong answer", correct: false},
+            {text: "wrong answer", correct: false},
+            {text: "right answer", correct: true},
+            {text: "wrong answer", correct: false},
+        ]
+    },
+
+    {
+        question: "What is Question 3"
+        answers: [
+            {text: "wrong answer", correct: false},
+            {text: "wrong answer", correct: false},
+            {text: "right answer", correct: true},
+            {text: "wrong answer", correct: false},
+        ]
+    },
+
+    {
+        question: "What is Question 4"
+        answers: [
+            {text: "wrong answer", correct: false},
+            {text: "wrong answer", correct: false},
+            {text: "right answer", correct: true},
+            {text: "wrong answer", correct: false},
+        ]
+    },
+
+    {
+        question: "What is Question 5"
+        answers: [
+            {text: "wrong answer", correct: false},
+            {text: "wrong answer", correct: false},
+            {text: "right answer", correct: true},
+            {text: "wrong answer", correct: false},
+        ]
+    },
+
+    {
+        question: "What is Question 6"
+        answers: [
+            {text: "wrong answer", correct: false},
+            {text: "wrong answer", correct: false},
+            {text: "right answer", correct: true},
+            {text: "wrong answer", correct: false},
+        ]
+    },
+
+    {
+        question: "What is Question 7"
+        answers: [
+            {text: "wrong answer", correct: false},
+            {text: "wrong answer", correct: false},
+            {text: "right answer", correct: true},
+            {text: "wrong answer", correct: false},
+        ]
+    },
+
+    {
+        question: "What is Question 8"
+        answers: [
+            {text: "wrong answer", correct: false},
+            {text: "wrong answer", correct: false},
+            {text: "right answer", correct: true},
+            {text: "wrong answer", correct: false},
+        ]
+    },
+
+    {
+        question: "What is Question 9"
+        answers: [
+            {text: "wrong answer", correct: false},
+            {text: "wrong answer", correct: false},
+            {text: "right answer", correct: true},
+            {text: "wrong answer", correct: false},
+        ]
+    },
+
+    {
+        question: "What is Question 10"
+        answers: [
+            {text: "wrong answer", correct: false},
+            {text: "wrong answer", correct: false},
+            {text: "right answer", correct: true},
+            {text: "wrong answer", correct: false},
+        ]
+    },
+]
